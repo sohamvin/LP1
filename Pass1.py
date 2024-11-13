5cf3d636940448d22000b8df85e561a654709c26
sohamvin <doyalkudo@gmail.com>
2024-11-13 12:00:45 +0530

    Added Explanation

--- Pass1.py
+++ Pass1.py
@@ -36,7 +36,186 @@ class Pass1:
         self.CurrentMacroName = ""
 
 
+#Helper functions
 
+
+    def PrintTables(self):
+        print("MNT: ")
+        for l, val in self.MNT.items():
+            print(l, " : ", val)
+
+        print("\n\n")
+
+        print("MDT: ")
+        for l in self.MDT:
+            print(l)
+
+        print("\n\n")
+
+        print("KPDT: ")
+        for l in self.KPDTab:
+            print(l)
+        
+        print("\n\n")
+
+        print("PNTAB: ")
+        for l, val in self.PNTabs.items():
+            print(l, " : ", val)
+
+
+    #get location like 1, 2, 3 of the parameter in the PNTab
+    def GetPositionKOfParameterInPNTABfor(self, parameter):
+        for k, arg in enumerate(self.PNTabs[self.CurrentMacroName]):
+            if arg == parameter:
+                return k
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+#Line analizing functions
+    def IdentifyMacro(self, i):
+        if "MACRO" in self.lines[i] and not self.InMacro:
+            #If this keyword was used then in the next line there should be the macro name and 
+            #Arguments defined so 
+
+            if i+1 >= len(self.lines): #if there are no more lines after the MACRO keyword, thats wrong
+                #No lines after the MACRO keyword:
+                print("WRONG CODE and use of MACRO Keyword")
+                return -1, False
+            
+            else:
+                self.InMacro = True
+                #We use the above guy for nested macro cases
+                macroLine = self.lines[i+1] # this is the line which has macro name and all that
+
+                if macroLine[0] in self.MNT: # 
+                    print("WRONG CODE macro name already used")
+                    return -1, False
+                
+                positionalArgs = 0 # num of positional args
+                keywordArgs = 0 # num of keyword args
+ 
+                data = [
+                    #Number of positional args/ number of keyword args/  MDT pointer/  KPDTab pointer
+                    0,                          0,                          0,          -1
+                ]
+
+                parameters = [
+
+                ] # add all parameters
+
+                keywordParameters = [
+
+                ] # add only keyword parameters
+                
+                macroName = "" 
+
+                for j, word in enumerate(macroLine): #analyze the macro defination line
+                    if j == 0:
+                        macroName = word # could have do this above / outside of loop also, i dont know whay i did this
+                        continue
+
+                    if "&" in word: # so it is indeed an argument cuz
+                        #MACRO_NAME     &ARG1      &ARG2    &ARG3=     &ARG4=AREG etc
+                        if "=" in word: # is keyword argument
+                            keywordArgs += 1
+                            parts = word.split("=")
+                            if parts[1] == "" or not parts[1]: # so if nothing after keyword argument (like ARG3)
+                                keywordParameters.append([parts[0], ""]) #Add default value as none
+                            else:
+                                keywordParameters.append([parts[0], parts[1]])
+                            
+                            parameters.append(parts[0])
+                            #We need to add the keyword argument nonetheless to PNTAB 
+
+                        else: # is positional argument
+                            positionalArgs += 1
+                            parameters.append(word)
+                            
+                        
+                    else:
+                        print("WRONG CODE, & is not used before parameters")
+                        return -1, False
+                
+                self.CurrentMacroName = macroName #name of the macro we trying to analyze
+                self.PNTabs[macroName] = parameters
+
+                data[0] = positionalArgs
+                data[1] = keywordArgs
+                data[2] = len(self.MDT) 
+                data[3] =  len(self.KPDTab) if keywordArgs != 0 else -1 # that is if keywordArgs are there then pointer, else -1 to indicate no keyword args
+
+                self.MNT[macroName] = data
+
+                self.KPDTab += keywordParameters 
+
+                return i+2, True
+        else:
+            #If MACRO keyword is not in the line, then just continue to the -> def AnalyzeLineByLine
+            return i, False
+        
+    
+    #For MEND we need special processing
+    def IdentifyMEND(self, i):
+        if "MEND" in self.lines[i]:
+            self.InMacro = False
+
+            self.MDT.append(["MEND"])
+
+            return i+1, True
+        else:
+            return i, False
+
+
+    def AnalyzeLine(self, i): #If neither MEND or MACRO Declaration line -> so append to MDT
+        replacement = []
+        for word in self.lines[i]:
+            if "&" in word: # so if we are using a parameter, we need to replace it with like (P,1) or (P,2) ...
+                if word in self.PNTabs[self.CurrentMacroName]:
+                    if self.GetPositionKOfParameterInPNTABfor(word) == -1:
+                        #something went wrong
+                        return -1
+                    else:
+                        replacement.append(f"(P,{self.GetPositionKOfParameterInPNTABfor(word)})")
+                else:
+                    print(f"WRONG CODE, {word} is not an argument in {self.CurrentMacroName}")
+                    return -1
+                
+                #A lot of error prevention code, which is not really necessary
+                
+            else:
+                replacement.append(word) # if it is not a parameter, then place that word as is
+                
+        self.MDT.append(replacement)
+        return i+1 # Go to next line
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Analyze line by line:
     def AnalyzeLineByLine(self):
         i = 0
 
@@ -44,10 +223,14 @@ class Pass1:
             print("lines: ", i, "\t", self.lines[i])
 
             i, wasStartLine = self.IdentifyMacro(i=i) 
+            #If Macro Start, special processing is done, continue to next lines
             if wasStartLine and i != -1:
                 continue
 
+            
+
             i, wasEndLine = self.IdentifyMEND(i=i)
+            #similar to macro start
             if wasEndLine and i != -1:
                 continue
 
@@ -58,33 +241,30 @@ class Pass1:
             if i == -1:
                 print("Error encountered in line analysis. Exiting.")
                 break
+                        # break from the k loop, the one trying to find the position of the word
 
-    def GetPositionKOfParameterInPNTABfor(self, parameter):
-        for k, arg in enumerate(self.PNTabs[self.CurrentMacroName]):
-            if arg == parameter:
-                return k
-        return -1
-                            # break from the k loop, the one trying to find the position of the word
 
-    def AnalyzeLine(self, i):
-        replacement = []
-        for word in self.lines[i]:
-            if "&" in word:
-                if word in self.PNTabs[self.CurrentMacroName]:
-                    if self.GetPositionKOfParameterInPNTABfor(word) == -1:
-                        return -1
-                    else:
-                        replacement.append(f"(P,{self.GetPositionKOfParameterInPNTABfor(word)})")
-                else:
-                    print(f"WRONG CODE, {word} is not an argument in {self.CurrentMacroName}")
-                    return -1
-                
-            else:
-                replacement.append(word) # if it is not a parameter, then place that word as is
-                
-        self.MDT.append(replacement)
-        return i+1
+
+
+
+
+
+
+
+
+
+
+#File reading
+    def ReadFile(self):
+        with open(self.fileName + ".txt", "r") as file:
+            for line in file:
+                line_part = [ part.strip(" ").replace(",", "") for part in line.strip().split(" ") if part]
+                if line_part:
+                    self.lines.append(line_part)
     
+
+
+
     def StoreTablesInFile(self):
         os.makedirs(self.fileName, exist_ok=True)
         with open(f"{self.fileName}/" + self.fileName + "MNT" + ".txt", "w") as file:
@@ -116,132 +296,18 @@ class Pass1:
                 file.write(array[0] + ":" + array[1])
                 file.write("\n")
             file.close()
-        
-
-
-    def IdentifyMacro(self, i):
-        if "MACRO" in self.lines[i] and not self.InMacro:
-
-            if i+1 >= len(self.lines):
-                #No lines after the MACRO keyword:
-                print("WRONG CODE and use of MACRO Keyword")
-                return -1, False
-            
-            else:
-                self.InMacro = True
-                macroLine = self.lines[i+1]
-
-                print(macroLine)
-
-                if macroLine[0] in self.MNT:
-                    print("WRONG CODE macro name already used")
-                    return -1, False
-                
-                positionalArgs = 0
-                keywordArgs = 0
-
-                data = [
-                    #Number of positional args/ number of keyword args/  MDT pointer/  KPDTab pointer
-                    0,                          0,                          0,          -1
-                ]
-
-                parameters = [
-
-                ]
-
-                keywordParameters = [
-
-                ]
-                
-                macroName = ""
-
-                for j, word in enumerate(macroLine):
-                    if j == 0:
-                        macroName = word
-                        continue
-
-                    if "&" in word:
-                        if "=" in word: # is keyword argument
-                            keywordArgs += 1
-                            parts = word.split("=")
-                            if parts[1] == "" or not parts[1]:
-                                keywordParameters.append([parts[0], ""])
-                            else:
-                                keywordParameters.append([parts[0], parts[1]])
-                            
-                            parameters.append(parts[0])
-
-                        else: # is positional argument
-                            positionalArgs += 1
-                            parameters.append(word)
-                            
-                        
-                    else:
-                        print("WRONG CODE, & is not used before parameters")
-                        return -1, False
-                
-                self.CurrentMacroName = macroName
-                self.PNTabs[macroName] = parameters
-
-                data[0] = positionalArgs
-                data[1] = keywordArgs
-                data[2] = len(self.MDT)
-                data[3] =  len(self.KPDTab) if keywordArgs != 0 else -1 # that is if keywordArgs are there then pointer, else -1 to indicate no args
-
-                self.MNT[macroName] = data
-
-                self.KPDTab += keywordParameters
-
-                return i+2, True
-        else:
-            return i, False
-        
-
-
-
-    def IdentifyMEND(self, i):
-        if "MEND" in self.lines[i]:
-            self.InMacro = False
-
-            self.MDT.append(["MEND"])
-
-            return i+1, True
-        else:
-            return i, False 
-        
-
-    def PrintTables(self):
-        print("MNT: ")
-        for l, val in self.MNT.items():
-            print(l, " : ", val)
-
-        print("\n\n")
-
-        print("MDT: ")
-        for l in self.MDT:
-            print(l)
-
-        print("\n\n")
-
-        print("KPDT: ")
-        for l in self.KPDTab:
-            print(l)
-        
-        print("\n\n")
-
-        print("PNTAB: ")
-        for l, val in self.PNTabs.items():
-            print(l, " : ", val)
-
-
-    def ReadFile(self):
-        with open(self.fileName + ".txt", "r") as file:
-            for line in file:
-                line_part = [ part.strip(" ").replace(",", "") for part in line.strip().split(" ") if part]
-                if line_part:
-                    self.lines.append(line_part)
     
 
+
+
+
+
+
+
+
+
+
+#Actual Functions to call
     def DoStuff(self):
         self.ReadFile()
         self.AnalyzeLineByLine()
--- Pass2.py
+++ Pass2.py
@@ -36,7 +36,7 @@ class PassTwo:
         #Not a dictionary because what will you do if:
         #M2 100, 200, &V=CREG, &U=BREG
         #M2 16, 120, &V=AREG, &U=BREG 
-        #here then there are two macro calls
+        #here then there are two macro calls for same macro name
         
 
         self.MacroExpandedCode = [
@@ -63,14 +63,51 @@ class PassTwo:
         ] # read the actual lines like: M1 10, 20, &B=CREG // or -- M2 100, 200, &V=AREG, &U=BREG 
         #to analyze them
 
+
+
+
+
+#Helper function
+    #Check if the line is a macro call or not
+    def IfMacroCall(self, line):
+        if len(line) < 1:
+            return False
+        if line[0] in self.MNT:
+            return True
+        return False
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+#Actual Functions
+
     def GenerateAPTab(self, macro_call_line):
 
         '''
          it is safe to assume that the assembler's pass 1 enforces 
          this rule and blocks or flags any incorrect ordering in macro 
-         definitions, ensuring that positional parameters come before 
-         keyword parameters. 
-         And we assme that is how they are stored as well in 
+         definitions, ensuring that 
+
+
+         !!!!!positional parameters come before keyword parameters!!!!!!. 
+         
+
+         And we assume that is how they are stored as well in 
          PNTab 
         '''
 
@@ -85,38 +122,53 @@ class PassTwo:
         KeywordParams = {
             
         }
-        if KPTPointer != -1:
-            for i in range(KPTPointer, KPTPointer+ numberOfKP):
-                KeywordParams[self.KPDTab[i][0]] = self.KPDTab[i][1]
 
-        parameters = self.PNTab[macroname]
+
+        if KPTPointer != -1: #So keyword parameters are there
+            for i in range(KPTPointer, KPTPointer+ numberOfKP):
+                KeywordParams[self.KPDTab[i][0]] = self.KPDTab[i][1] 
+
+        #generate the KeywordParam datastructure which has keyword parameters and default vals 
+        #for this macro whose name is -> macroname
+
+        parameters = self.PNTab[macroname] #get PNTab for the macro
 
         APTab = {
-
+            #Maps macro Paremeter to argument passed/ default argument
         }
-        ArgsGiven = macro_call_line[1:]
+
+
+        ArgsGiven = macro_call_line[1:] # as first element if macro name, all rest are parameters
 
         if len(ArgsGiven) > len(parameters) or len(ArgsGiven) < numberOfPP:
             print("WRONG CODE, Wrong number of positional parameters")
             return -1, {}
 
+        #Example:
+        #Macro -> INCR_M &ARG1, &ARG2, &ARG3=, &ARG4=AREG, &ARG5=
+        #Macro call -> 1) INCR_M BREG, CREG, AREG, &ARG5=DREG --> this is valid
+        #Macro call -> 2) INCR_M BREG, CREG, &ARGE5= AREG --> Invalid
 
         for j in range(len(ArgsGiven)):
-            if "=" in ArgsGiven[j]:
+            if "=" in ArgsGiven[j]: # so if it is a keyword argument
                 parts = ArgsGiven[j].split("=")
                 if parts[0] not in KeywordParams:
                     print("WRONG CODE, No such keyword param ", parts[0])
                     return -1, {}
                 else:
                     APTab[parts[0]] = parts[1]
-            else:
+            else:#jth parameter of APTab maps the jth argument, 
+                #True for positional obviously but maybe also for keyword arguments passed in that order 
+                #So ARG1, and ARG2 are mapped but 
+                #ARG3 is also mapped to AREG for 1)
                 APTab[parameters[j]] = ArgsGiven[j]    
 
         #Finish of remaining keyword parameters missed in loop
 
         for key, val in KeywordParams.items():
             if key not in APTab:
-                if val == '':
+                if val == '': # if keyword argument not already passed and that keyword parameter doesnt have a default value then wrong.
+                    #So for 2) &AREG3 is not in APTab and it doesnt have a default val so wrong argument passing
                     print("WRONG ARGUMENT PASSING, ", key, " Keyword argument wasnt initialized")
                     return -1, {}
                 else:
@@ -125,66 +177,33 @@ class PassTwo:
         return 0, APTab
 
 
-    def PrintDataStructres(self):
-        # print("PNTAB")
-        # for key, val in self.PNTab.items():
-        #     print(key, " : ", val)
-        
 
-        # print("\n\n\nMNTab")
-        # for key, val in self.MNT.items():
-        #     print(key, " : ", val)
-
-
-        # print("\n\n\nKPDTab")
-        # for ele in self.KPDTab:
-        #     print(ele)
-
-        # print("\n\n\nLines Read From Pass2")
-        # for ele in self.ReadPassTwoLines:
-        #     print(ele)
-
-        # print("\n\n\n\n\nMacro Defination Table")
-        # for ele in self.MDT:
-        #     print(ele)
-
-        print("\n\n\n\n\n\nLines Read")
-        for ele in self.ReadPassTwoLines:
-            print(ele)
-
-        print("\n\n\n\n\n\nExpanded Code")
-        for ele in self.MacroExpandedCode:
-            print(ele) 
-
-
-
-
-    def IfMacroCall(self, line):
-        if len(line) < 1:
-            return False
-        if line[0] in self.MNT:
-            return True
-        return False
-    
-
-
+    #Given that you have the APTAb and the macro name, generate the 
+    #Code for that
     def ProduceCodeForAPTabAndMacro(self, macroname, APtab):
         ParameterToPosition = {
 
         }
+        #maps parameter to position like:
+        #1 : &AREG1
+        #2 : &AREG2             .....etc
 
         for j, param in enumerate(self.PNTab[macroname]):
             ParameterToPosition[j] = param # position -> name maping
 
-        
-        MDTForMacro = []
 
-        for i in range(self.MNT[macroname][2], len(self.MDT)):
-            if "MEND" in self.MDT[i]:
+        
+        MDTForMacro = [] #what is the macro expansion looks like for that macro call
+
+        for i in range(self.MNT[macroname][2], len(self.MDT)): # self.MNT[macroname][2] is MDT pointer
+            if "MEND" in self.MDT[i]: #So we done
                 break
             line = []
             for ele in self.MDT[i]:
-                if "(" in ele:
+                if "(" in ele: # if '(' in the string then prolly
+                    #ADD (P,1)  (P,2)
+                    #And ele = (P,1) or (P,2)
+                    #So we need to replace P,1 and P,2 with the APTab values 
                     position = ele.strip("(").strip(")").split(",")[1]
                     if position.isdigit():
                         position = int(position)
@@ -193,6 +212,7 @@ class PassTwo:
                         print("WRONG CODE,", position)
                         return -1
                 else:
+                    #Otherwise append the string as is
                     line.append(ele)
         
             MDTForMacro.append(line)
@@ -201,29 +221,20 @@ class PassTwo:
         
 
 
-    def DoStuff(self):
-        self.ReadFiles()
-
-        for line in self.ReadPassTwoLines:
-
-            if self.IfMacroCall(line=line):
-
-                num, aptab = self.GenerateAPTab(line)
-
-                print(f"{line[0]} : {aptab}\n\n\n")
-                if num == -1:
-                    return
-                self.MacroExpandedCode += self.ProduceCodeForAPTabAndMacro(macroname=line[0], APtab=aptab)
-                    
-            else:
-                self.MacroExpandedCode.append(line)
-
-        self.WriteExpandedCodeToFile()
-                
-            
 
 
 
+
+
+
+
+
+
+
+
+
+
+#File reading code and file writing code
     def ReadFiles(self):
         with open(f"{self.filename}/" +self.filename + "KPDTAB" + ".txt", "r") as file:
             for line in file:
@@ -271,6 +282,76 @@ class PassTwo:
                 file.write("\n")
             
             file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Functions to call
+    def DoStuff(self):
+        self.ReadFiles()
+
+        for line in self.ReadPassTwoLines:
+
+            if self.IfMacroCall(line=line): #Check if line is a macro call
+
+                num, aptab = self.GenerateAPTab(line) #Generate AP tab for the macro
+
+                print(f"{line[0]} : {aptab}\n\n\n")
+                if num == -1:
+                    #Summin went wrong
+                    return
+                self.MacroExpandedCode += self.ProduceCodeForAPTabAndMacro(macroname=line[0], APtab=aptab) #Add the expanded code for the macro call
+                    
+            else:
+                self.MacroExpandedCode.append(line) #If no macro call, add line ass is
+
+        self.WriteExpandedCodeToFile()
+                
+            
+    def PrintDataStructres(self):
+        # print("PNTAB")
+        # for key, val in self.PNTab.items():
+        #     print(key, " : ", val)
+        
+
+        # print("\n\n\nMNTab")
+        # for key, val in self.MNT.items():
+        #     print(key, " : ", val)
+
+
+        # print("\n\n\nKPDTab")
+        # for ele in self.KPDTab:
+        #     print(ele)
+
+        # print("\n\n\nLines Read From Pass2")
+        # for ele in self.ReadPassTwoLines:
+        #     print(ele)
+
+        # print("\n\n\n\n\nMacro Defination Table")
+        # for ele in self.MDT:
+        #     print(ele)
+
+        print("\n\n\n\n\n\nLines Read")
+        for ele in self.ReadPassTwoLines:
+            print(ele)
+
+        print("\n\n\n\n\n\nExpanded Code")
+        for ele in self.MacroExpandedCode:
+            print(ele) 
+
+
 
 
 
